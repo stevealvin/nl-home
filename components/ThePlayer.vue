d0186729755724c6ac1c5e88d6c388858d460711
chenzewen <1935338255@139.com>
2024-09-26 11:38:33 +0800

    fix:

--- components/ThePlayer.vue
+++ components/ThePlayer.vue
@@ -1,32 +1,32 @@
 <script setup>
-import DPlayer from 'dplayer'
-import Hls from 'hls.js'
+// import DPlayer from 'dplayer'
+// import Hls from 'hls.js'
 
 const props = defineProps({
   src: String,
   autoplay: Boolean,
 })
 
-onMounted(() => {
-  const dp = new DPlayer({
-      container: document.getElementById('dplayer'),
-      autoplay: true,
-      video: {
-        autoplay: true,
-        url: props.src,
-        // url: 'http://liveop.cctv.cn/hls/4KHD/playlist.m3u8',
-        // url: 'https://t21.cdn2020.com/video/m3u8/2023/04/13/6b00abe1/index.m3u8',
-        type: 'hls',
-        customType: {
-          hls: function (video, player) {
-            const hls = new Hls()
-            hls.loadSource(player.options.video.url)
-            hls.attachMedia(video)
-          },
-        },
-      },
-  })
-})
+// onMounted(() => {
+//   const dp = new DPlayer({
+//       container: document.getElementById('dplayer'),
+//       autoplay: true,
+//       video: {
+//         autoplay: true,
+//         url: props.src,
+//         // url: 'http://liveop.cctv.cn/hls/4KHD/playlist.m3u8',
+//         // url: 'https://t21.cdn2020.com/video/m3u8/2023/04/13/6b00abe1/index.m3u8',
+//         type: 'hls',
+//         customType: {
+//           hls: function (video, player) {
+//             const hls = new Hls()
+//             hls.loadSource(player.options.video.url)
+//             hls.attachMedia(video)
+//           },
+//         },
+//       },
+//   })
+// })
 
 
 </script>
